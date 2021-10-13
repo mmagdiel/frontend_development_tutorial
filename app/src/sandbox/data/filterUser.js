3a78b99af95b72943e099a7880d602807b2086b9
andreaguerrero8 <andreaguerrero.ar@gmail.com>
2021-10-13 12:48:23 -0500

    se suben cambios en ejercicios

--- app/src/sandbox/data/filterUser.js
+++ app/src/sandbox/data/filterUser.js
@@ -1,13 +1,17 @@
-import { posts } from './mock';
+import { posts } from './mock.js';
 
-function getPostByUserId() {
-  return posts;
+function getPostByUserId(userId) {
+return posts.filter(item => item.userId === userId);
 }
-function getPostById() {
-  return 0;
+
+
+function getPostById(ID) {
+  return posts.filter(item => item.id === ID);
 }
-function getPostByUserIdAndId() {
-  return 0;
+
+
+function getPostByUserIdAndId(userId, Id) {
+  return posts.filter(item => item.userId === userId && item.id === Id);
 }
 
 export { getPostByUserId, getPostById, getPostByUserIdAndId };
